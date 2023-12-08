3f676b95e7bac5a17c88c67549e3b2ec0b777ada
tungUAS <Tung.Trinh@senacor.com>
2023-12-08 21:33:26 +0100

    change service pod

--- src/modules/axios.ts
+++ src/modules/axios.ts
@@ -5,13 +5,12 @@ import type { AxiosResponse } from "axios";
 const getAxiosWithBasicAuth = async <T>(
   url: string,
   token: string,
-  defaultValue: T | undefined = undefined,
 ): Promise<T | undefined> => {
   try {
     const response: AxiosResponse<T> = await axios.get(url, { headers: { Authorization: token } });
     return response.data;
   } catch (error) {
-    return defaultValue;
+    return undefined;
   }
 };
 
