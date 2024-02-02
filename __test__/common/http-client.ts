06785070983668eb5b124f0ac47cf1b03afcbbd9
tungUAS <Tung.Trinh@senacor.com>
2024-02-02 23:51:47 +0100

    add integration test docker compose

--- __test__/common/http-client.ts
+++ __test__/common/http-client.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 import type { AxiosRequestConfig } from "axios";
 
+const PORT = process.env["SERVICE_PORT"] || 8080;
+
 export const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: `http://localhost:${PORT}`,
   validateStatus: () => true,
 });
 
