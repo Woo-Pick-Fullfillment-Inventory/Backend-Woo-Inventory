8f04536fc6ac446c61aabebb4778077b30727e55
tungUAS <Tung.Trinh@senacor.com>
2024-02-24 14:51:35 +0100

    remove test in cicd

--- src/repository/firestore/index.ts
+++ src/repository/firestore/index.ts
@@ -1,11 +1,14 @@
 import { initializeAdminApp } from "@firebase/rules-unit-testing";
-import admin from "firebase-admin";
+import { Firestore } from "@google-cloud/firestore";
 
 import { getUserByAttributeFactory } from "./get-user.js";
 import { insertUserFactory } from "./insert-user.js";
 
 export const firestoreClient: FirebaseFirestore.Firestore = process.env["NODE_ENV"] === "production"
-  ? admin.firestore()
+  ? new Firestore({
+    projectId: process.env["PROJECT_ID"],
+    keyFilename: process.env["GOOGLE_APPLICATION_CREDENTIALS"],
+  })
   : initializeAdminApp({ projectId: "test-project" }).firestore();
 
 export const getUserByAttribute = getUserByAttributeFactory(firestoreClient);
