a0e61eb00a2fab8d43b1b37b0353ee165112cded
tungUAS <Tung.Trinh@senacor.com>
2024-02-29 20:10:05 +0100

    add image src

--- src/modules/create-logger.ts
+++ src/modules/create-logger.ts
@@ -3,9 +3,8 @@ import winston from "winston";
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
-    winston.format.timestamp(),
     winston.format.printf(
-      (info) => `${info["timestamp"]} ${info.level}: ${info.message}`,
+      (info) => `${info.level}: ${info.message}`,
     ),
   ),
   transports: [ new winston.transports.Console() ],
