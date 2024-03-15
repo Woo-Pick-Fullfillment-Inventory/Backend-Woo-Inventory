806524cc9ee46c82c62b285dcc15d569f24c917c
tungUAS <Tung.Trinh@senacor.com>
2024-03-15 15:06:22 +0100

    create logger for cloud format

--- src/modules/create-logger.ts
+++ src/modules/create-logger.ts
@@ -1,29 +1,36 @@
 import colors from "cli-color";
 import winston from "winston";
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.printf(
-      (info) => {
-        const levelColor = (level: string) => {
-          switch (level) {
-            case "info":
-              return colors.cyan(level);
-            case "warn":
-              return colors.yellow(level);
-            case "error":
-              return colors.red.bold(level);
-            default:
-              return level;
-          }
-        };
+const logger =
+  process.env["NODE_ENV"] !== "production"
+    ? winston.createLogger({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.printf((info) => {
+          const levelColor = (level: string) => {
+            switch (level) {
+              case "info":
+                return colors.cyan(level);
+              case "warn":
+                return colors.yellow(level);
+              case "error":
+                return colors.red.bold(level);
+              default:
+                return level;
+            }
+          };
 
-        return `${levelColor(info.level)}: ${info.message}`;
-      },
-    ),
-  ),
-  transports: [ new winston.transports.Console() ],
-});
+          return `${levelColor(info.level)}: ${info.message}`;
+        }),
+      ),
+      transports: [ new winston.transports.Console() ],
+    })
+    : winston.createLogger({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.printf((info) => `${info.level}: ${info.message}`),
+      ),
+      transports: [ new winston.transports.Console() ],
+    });
 
 export default logger;
