713254abc5c25afd2f9c539634d424c2e0af8c8e
tungUAS <Tung.Trinh@senacor.com>
2024-03-19 00:11:52 +0100

    bypass undefined

--- src/repository/firestore/index.ts
+++ src/repository/firestore/index.ts
@@ -10,20 +10,12 @@ import {
 } from "./update-user.js";
 dotenv.config();
 
-if (!process.env["PROJECT_ID"]) {
-  throw new Error("PROJECT_ID environment variable is required");
-}
-
-if (!process.env["FIRESTORE_PORT"]) {
-  throw new Error("FIRESTORE_PORT environment variable is required");
-}
-
 export const firestoreClient: FirebaseFirestore.Firestore = process.env["NODE_ENV"] === "production"
-  ? new Firestore({ projectId: process.env["PROJECT_ID"] })
+  ? new Firestore({ projectId: process.env["PROJECT_ID"] as string })
   : new Firestore({
-    projectId: process.env["PROJECT_ID"],
+    projectId: process.env["PROJECT_ID"] as string,
     host: "127.0.0.1",
-    port: parseInt(process.env["FIRESTORE_PORT"]),
+    port: parseInt(process.env["FIRESTORE_PORT"] as string),
     ssl: false,
   });
 
