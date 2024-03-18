1da7b9797559bc8e100da5e32744816f6d0139eb
tungUAS <Tung.Trinh@senacor.com>
2024-03-18 21:54:03 +0100

    trigger pipeline

--- src/repository/firestore/index.ts
+++ src/repository/firestore/index.ts
@@ -1,4 +1,3 @@
-import { initializeAdminApp } from "@firebase/rules-unit-testing";
 import { Firestore } from "@google-cloud/firestore";
 
 import { batchWriteProductsFactory } from "./batch-write-products.js";
@@ -9,13 +8,22 @@ import {
   updateUserProductsSyncedFactory,
 } from "./update-user.js";
 
-if (process.env["NODE_ENV"] === "production" && !process.env["PROJECT_ID"]) {
+if (!process.env["PROJECT_ID"]) {
   throw new Error("PROJECT_ID is not defined");
+}
+
+if (!process.env["FIRESTORE_PORT"]) {
+  throw new Error("FIRESTORE_PORT is not defined");
 }
 
 export const firestoreClient: FirebaseFirestore.Firestore = process.env["NODE_ENV"] === "production"
   ? new Firestore({ projectId: process.env["PROJECT_ID"] as string })
-  : initializeAdminApp({ projectId: "test-project" }).firestore();
+  : new Firestore({
+    projectId: process.env["PROJECT_ID"],
+    host: "127.0.0.1",
+    port: parseInt(process.env["FIRESTORE_PORT"]),
+    ssl: false,
+  });
 
 export const getUserByAttribute = getUserByAttributeFactory(firestoreClient);
 export const insertUser = insertUserFactory(firestoreClient);
