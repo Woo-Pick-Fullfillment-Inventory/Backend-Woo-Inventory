ebab3d12ef1e4c560cfbffb8e993252aff21491b
tungUAS <Tung.Trinh@senacor.com>
2024-03-25 16:31:38 +0100

    hardcode secret for testing purpose

--- __test__/common/create-authorization-header.ts
+++ __test__/common/create-authorization-header.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 dotenv.config();
 
+const secret = process.env["JWT_SECRET"] || "secret";
+
 export const createAuthorizationHeader = (userId: string): string => {
-  return `Bearer ${jwt.sign({ userId }, process.env["JWT_SECRET"] as string)}`;
+  return `Bearer ${jwt.sign({ userId }, secret)}`;
 };
