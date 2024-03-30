80a01707d01b2b81691c1640c748d45f01089439
khai-pi <khaiphamhoang.pi@gmail.com>
2024-03-30 22:26:49 +0100

    test: add check products length add product

--- __test__/business/add-product.integration.test.ts
+++ __test__/business/add-product.integration.test.ts
@@ -1,10 +1,11 @@
 import { WireMockRestClient } from "wiremock-rest-client";
 
+import { firestoreRepository } from "../../src/repository/firestore";
 import { createAuthorizationHeader } from "../common/create-authorization-header.js";
 import { httpClient } from "../common/http-client";
 import { mockUserForSyncingProducts } from "../common/mock-data.js";
-// import { firestoreRepository } from "../../src/repository/firestore";
-// import type { ProductsType } from "../../src/repository/woo-api/models";
+
+import type { ProductsType } from "../../src/repository/woo-api/models";
 
 const woocommerceApiMockServer = new WireMockRestClient(
   "http://localhost:1080",
@@ -15,7 +16,7 @@ describe("Get products test", () => {
     await woocommerceApiMockServer.requests.deleteAllRequests();
   });
 
-  it("should return 201", async () => {
+  it("should inscrease products count", async () => {
     const newProduct = {
       name: "Premium Quality",
       sku: "some_sku",
@@ -27,25 +28,11 @@ describe("Get products test", () => {
       ],
     };
 
+    const oldProducts = await firestoreRepository.collection.viewCollection<ProductsType>(`users-products/users-${mockUserForSyncingProducts.user_id}-products/products`);
     const response = await httpClient.post("api/v1/products", newProduct, { headers: { authorization: createAuthorizationHeader(mockUserForSyncingProducts.user_id) } });
+    const newProducts = await firestoreRepository.collection.viewCollection<ProductsType>(`users-products/users-${mockUserForSyncingProducts.user_id}-products/products`);
+    expect(newProducts.length).toEqual(oldProducts.length + 1);
 
     expect(response.status).toBe(201);
   });
-
-  // it("should inscrease products count", async () => {
-  //   const newProduct = {
-  //     name: "Premium Quality",
-  //     sku: "some_sku",
-  //     price: "100",
-  //     stock_quantity: 200,
-  //     images: [
-  //       { src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg" },
-  //       { src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg" },
-  //     ],
-  //   };
-  //   const oldProducts = await firestoreRepository.collection.viewCollection<ProductsType>(`users-products/users-${mockUserForSyncingProducts.user_id}-products/products`);
-  //   await httpClient.post("api/v1/products", newProduct, { headers: { authorization: createAuthorizationHeader(mockUserForSyncingProducts.user_id) } });
-  //   const newProducts = await firestoreRepository.collection.viewCollection<ProductsType>(`users-products/users-${mockUserForSyncingProducts.user_id}-products/products`);
-  //   expect(newProducts.length).toEqual(oldProducts.length + 1);
-  // })
 });
