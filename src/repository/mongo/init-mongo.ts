f2e5c2a9bef073aa765b591f4c8a63318b887467
tungUAS <Tung.Trinh@senacor.com>
2024-05-08 19:10:31 +0200

    hardcode url

--- src/repository/mongo/init-mongo.ts
+++ src/repository/mongo/init-mongo.ts
@@ -1,24 +1,17 @@
 import { MongoClient } from "mongodb";
 
-const username = process.env["MONGO_INITDB_ROOT_USERNAME"] || "admin";
-const password = process.env["MONGO_INITDB_ROOT_PASSWORD"] || "pass";
-const host = process.env["MONGO_HOST"] || "mongo";
-const port = process.env["MONGO_PORT"] || "27017";
-const database = process.env["MONGO_INITDB_DATABASE"] || "test-database";
+const mongoUri =
+  "mongodb://admin:pass@mmongo:27017/test-database?retryWrites=true&writeConcern=majority&authSource=admin";
 
-// todo: add options
-const mongoClient = new MongoClient(
-  `mongodb://${username}:${password}@${host}:${port}/${database}?retryWrites=true&writeConcern=majority&authSource=admin`,
-  { connectTimeoutMS: 30000 },
-);
+const mongoClient = new MongoClient(mongoUri);
 
 mongoClient
   .on("error", (err) => console.error("MongoDB Client Error:", err))
   .on("connect", () => console.log("MongoDB Client connecting ..."))
+  .on("open", () => console.log("MongoDB Client connected!"))
   .on("close", () => console.log("MongoDB Client connection closed."))
   .on("reconnect", () => console.log("MongoDB Client reconnected."))
   .on("timeout", () => console.warn("MongoDB Client connection timeout."))
-  .on("disconnected", () => console.warn("MongoDB Client disconnected."))
-  .connect();
+  .on("disconnect", () => console.warn("MongoDB Client disconnected."));
 
 export default mongoClient;
