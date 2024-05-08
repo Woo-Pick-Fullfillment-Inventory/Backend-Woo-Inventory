6919363845215f7a2d51ea3b7064cecd16752480
tungUAS <Tung.Trinh@senacor.com>
2024-05-08 17:37:56 +0200

    hardcode env var

--- src/repository/mongo/init-mongo.ts
+++ src/repository/mongo/init-mongo.ts
@@ -1,7 +1,16 @@
 import { MongoClient } from "mongodb";
 
+const username = process.env["MONGO_INITDB_ROOT_USERNAME"] || "admin";
+const password = process.env["MONGO_INITDB_ROOT_PASSWORD"] || "password";
+const host = process.env["MONGO_HOST"] || "mongo";
+const port = process.env["MONGO_PORT"] || "27017";
+const database = process.env["MONGO_INITDB_DATABASE"] || "test-database";
+
 // todo: add options
-const mongoClient = new MongoClient(process.env["MONGO_URI"] as string);
+const mongoClient = new MongoClient(
+  `mongodb://${username}:${password}@${host}:${port}/${database}?retryWrites=true&writeConcern=majority&authSource=admin`,
+  { connectTimeoutMS: 30000 },
+);
 
 mongoClient
   .on("error", (err) => console.error("MongoDB Client Error:", err))
